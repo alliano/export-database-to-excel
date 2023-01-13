2b209635e9b836de0d17a9383727b7a13ba5f59c
alliano <allianoanonymous@gmail.com>
2023-01-13 11:58:19 +0900

    create Class utility to convert file

---
+++ src/main/java/com/export/utils/ConvertFile.java
@@ -0,0 +1,29 @@
+package com.export.utils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ConvertFile {
+
+    public static File convert(MultipartFile multipartFile){
+        File covertFile = new File(System.getProperty("user.dir")+"/src/main/java/com/export/tmp/"+UUID.randomUUID().toString()+".xlsx");
+        try {
+            covertFile.createNewFile();
+            FileOutputStream outputStrem = new FileOutputStream(covertFile);
+            outputStrem.write(multipartFile.getBytes());
+            outputStrem.close();
+            log.info("success write file");
+        } catch (IOException IOX) {
+            covertFile = null;
+            throw new RuntimeException(IOX.getMessage());
+        }
+        return covertFile;
+    }
+}
