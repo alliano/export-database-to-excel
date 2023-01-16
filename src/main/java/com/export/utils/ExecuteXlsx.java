597d4bddafce2cef6a2cf86cc0f5328226eb2c4b
alliano <allianoanonymous@gmail.com>
2023-01-16 16:03:30 +0900

    repair some bugs

--- src/main/java/com/export/utils/ExecuteXlsx.java
+++ src/main/java/com/export/utils/ExecuteXlsx.java
@@ -4,8 +4,10 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.sql.DataSource;
 
@@ -24,51 +26,55 @@ public class ExecuteXlsx {
 
     private final DataSource dataSource;
 
-    public void createSchema(List<String> patren) throws SQLException{
+    public void createSchema(List<String> tableHeader, List<DataType> dataType, String tableName) throws SQLException{
         Connection connection = dataSource.getConnection();
         Statement statement = connection.createStatement();
-        String ddl = """
-                """;
+        tableHeader = tableHeader.stream().map(l -> {
+            String[] replaceSpace = l.split(" ");
+            return Arrays.asList(replaceSpace).stream().reduce(( first, last ) -> {
+                return first.concat("_").concat(last);
+            }).get();
+        }).collect(Collectors.toList());
+        String ddl = SqlUtil.ddlBuilder(tableHeader, dataType, tableName);
         statement.execute(ddl);
         connection.close();
         statement.close();
     }
 
-    public void batchInsert(List<List<String>> bodyOfTable){
-
-    }
-
     public TableDto sperate(XSSFSheet sheet){
-        // ubah mejadi iterator untuk di loop
         Iterator<Row> rowIterator = sheet.iterator();
         List<String> header = new ArrayList<>();
-        // select headernya
         Row headerRow = rowIterator.next();
-        // masukan tabel headernya ke variabel header
-        headerRow.forEach(h -> header.add(h.getStringCellValue()));
         TableDto table = new TableDto();
+        headerRow.forEach(h -> header.add(h.getStringCellValue()));
+        Row takeDataType = rowIterator.next();
+        takeDataType.forEach(c -> {
+            if(c.getCellType() == CellType.STRING ) table.addDataType(DataType.STRING);
+            else if(c.getCellType() == CellType.NUMERIC ) table.addDataType(DataType.INTEGER);
+            else if(c.getCellType() == CellType.BOOLEAN) table.setDataType(List.of(DataType.BOLLEAN));
+        });
         List<List<String>> body = new ArrayList<List<String>>();
-        // loop body tabel nya
         rowIterator.forEachRemaining(r -> {
             List<String> dataList = new ArrayList<String>();
             Iterator<Cell> cellIterator =  r.iterator();
-            // loop cell nya
             cellIterator.forEachRemaining(c -> {
-
                 if(c.getCellType() == CellType.NUMERIC){
-                    table.setDataType(List.of(DataType.INTEGER));
-
+                    String intToStr = String.valueOf(c.getNumericCellValue());
+                    dataList.add(intToStr);
                 }
                 else if(c.getCellType() == CellType.STRING) {
-                    table.setDataType(List.of(DataType.STRING));
                     dataList.add(c.getStringCellValue());
                 }
             });
                 body.add(dataList);
-            });
+        });
         body.removeIf(data -> data.isEmpty());
         table.setHeader(header);
         table.setBody(body);
         return table;
+    }
+
+    public void batchInsert(List<List<String>> bodyOfTable){
+
     }
 }
