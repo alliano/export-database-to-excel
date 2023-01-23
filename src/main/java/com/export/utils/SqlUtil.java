21485679160c84aed24f773ea435241d149cb9a2
alliano <allianoanonymous@gmail.com>
2023-01-23 20:11:36 +0900

    create a utility class to generate ddl and dml

--- src/main/java/com/export/utils/SqlUtil.java
+++ src/main/java/com/export/utils/SqlUtil.java
@@ -1,21 +1,32 @@
 package com.export.utils;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SqlUtil {
 
-    public static String ddlBuilder(List<String> tableHeader, List<DataType> dataType, String tableName){
-        String[] replaceSpace = tableName.split(" ");
-        tableName = Arrays.asList(replaceSpace).stream().reduce(( first, last ) -> {
-            return first.concat("_").concat(last);
-        }).get();
+    public String ddlBuilder(List<String> tableHeader, List<DataType> dataType, String tableName){
+        tableHeader.removeIf(d -> d.isEmpty() || d.equalsIgnoreCase("id"));
+        tableName = FormatterString.toSnakeCase(tableName);
+        tableHeader = FormatterString.toSnakeCase(tableHeader);
         List<String> prepareStmt = new ArrayList<String>();
-        prepareStmt.add("CREATE TABLE "+tableName+"(");
+        prepareStmt.add("CREATE TABLE ".concat(tableName).concat("("));
         for (int i = 0; i < tableHeader.size(); i++) {
             if(dataType.get(i) == DataType.STRING) {
-                prepareStmt.add(" ".concat(tableHeader.get(i).concat(" VARCHAR(100)")));
+                if(tableHeader.get(i).matches("^description")||tableHeader.get(i).matches("[a-zA-Z_]+description*")) {
+                    prepareStmt.add(" ".concat(tableHeader.get(i).concat(" VARCHAR(500)")));
+                }
+                else if(tableHeader.get(i).equalsIgnoreCase("name") || tableHeader.get(i).equalsIgnoreCase("email")){
+                    prepareStmt.add(" ".concat(tableHeader.get(i).concat(" VARCHAR(50)")));
+                }
+                else {
+                    prepareStmt.add(" ".concat(tableHeader.get(i).concat(" VARCHAR(255)")));
+                }
             }
             else if(dataType.get(i) == DataType.INTEGER){
                 prepareStmt.add(" ".concat(tableHeader.get(i).concat(" INT NOT NULL")));
@@ -24,9 +35,43 @@ public class SqlUtil {
                 prepareStmt.add(" ".concat(tableHeader.get(i).concat(" BOOLEAN NOT NULL")));
             }
         }
-        String stmt = prepareStmt.toString().replaceFirst("]", ");");
-        String rep = stmt.replace("[", "");
-        String ddl = rep.replaceFirst(",", "id INT NOT NULL PRIMARY KEY,");
-        return ddl;
+        if(!prepareStmt.contains("id")) prepareStmt.add(1, "id SERIAL NOT NULL PRIMARY KEY ");
+        else
+        prepareStmt.stream().map( d -> {
+            if(d == "id") {
+                d.concat("SERIAL NOT NULL PRIMARY KEY ");
+            }
+            return d;
+        });
+        return prepareStmt.toString().replaceAll("[\\[\\]]", "").replaceFirst(",", "").concat(");");
     }
+
+    public String batchInsert(List<List<String>> bodyOfTable, String entityName, List<String> tableHeader) {
+        String dml = "";
+        for (List<String> list : bodyOfTable) {
+            list = list.stream().map( d -> {
+               if(Pattern.compile("(true|false)",Pattern.CASE_INSENSITIVE).matcher(d).matches()){
+                    return d;
+                }
+                else if(Pattern.compile("^[0-9]+$").matcher(d).matches()){
+                    return d;
+                }
+                else {
+                    try {
+                        int parseInt = Integer.parseInt(String.valueOf(((int)Double.parseDouble(d))));
+                        return String.valueOf(parseInt);
+                    } catch (Exception e) {
+                        return "\'"+d+"\'";
+                    }
+                }
+            }).collect(Collectors.toList());
+            String data = list.toString().replaceAll("[\\[]", "(").replaceAll("[\\]]", "),");
+            dml = dml.concat(data);
+        }
+        return "INSERT INTO ".concat(entityName).concat(FormatterString.toSnakeCase(tableHeader).toString().replaceAll("[\\[]", "(")
+        .replaceAll("[\\]]", ")")).concat(" VALUES")
+        .concat(dml.replaceAll("[^a-zA-Z0-9@\\(\\)\\.\\s]$", ";"));
+    }
+
+
 }
