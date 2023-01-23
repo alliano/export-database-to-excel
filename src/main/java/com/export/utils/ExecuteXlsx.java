666cbdd385103154667e452182399ef7263cccef
alliano <allianoanonymous@gmail.com>
2023-01-23 20:09:45 +0900

    fix some bug

--- src/main/java/com/export/utils/ExecuteXlsx.java
+++ src/main/java/com/export/utils/ExecuteXlsx.java
@@ -4,20 +4,18 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.sql.DataSource;
 
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.springframework.context.annotation.Configuration;
 
 import com.export.dtos.TableDto;
+
 import lombok.AllArgsConstructor;
 
 
@@ -26,16 +24,12 @@ public class ExecuteXlsx {
 
     private final DataSource dataSource;
 
+    private final SqlUtil sqlUtil;
+
     public void createSchema(List<String> tableHeader, List<DataType> dataType, String tableName) throws SQLException{
         Connection connection = dataSource.getConnection();
         Statement statement = connection.createStatement();
-        tableHeader = tableHeader.stream().map(l -> {
-            String[] replaceSpace = l.split(" ");
-            return Arrays.asList(replaceSpace).stream().reduce(( first, last ) -> {
-                return first.concat("_").concat(last);
-            }).get();
-        }).collect(Collectors.toList());
-        String ddl = SqlUtil.ddlBuilder(tableHeader, dataType, tableName);
+        String ddl = this.sqlUtil.ddlBuilder(tableHeader, dataType, tableName);
         statement.execute(ddl);
         connection.close();
         statement.close();
@@ -44,37 +38,62 @@ public class ExecuteXlsx {
     public TableDto sperate(XSSFSheet sheet){
         Iterator<Row> rowIterator = sheet.iterator();
         List<String> header = new ArrayList<>();
-        Row headerRow = rowIterator.next();
         TableDto table = new TableDto();
-        headerRow.forEach(h -> header.add(h.getStringCellValue()));
-        Row takeDataType = rowIterator.next();
-        takeDataType.forEach(c -> {
-            if(c.getCellType() == CellType.STRING ) table.addDataType(DataType.STRING);
-            else if(c.getCellType() == CellType.NUMERIC ) table.addDataType(DataType.INTEGER);
-            else if(c.getCellType() == CellType.BOOLEAN) table.setDataType(List.of(DataType.BOLLEAN));
-        });
         List<List<String>> body = new ArrayList<List<String>>();
-        rowIterator.forEachRemaining(r -> {
-            List<String> dataList = new ArrayList<String>();
-            Iterator<Cell> cellIterator =  r.iterator();
-            cellIterator.forEachRemaining(c -> {
-                if(c.getCellType() == CellType.NUMERIC){
-                    String intToStr = String.valueOf(c.getNumericCellValue());
-                    dataList.add(intToStr);
-                }
-                else if(c.getCellType() == CellType.STRING) {
-                    dataList.add(c.getStringCellValue());
-                }
-            });
-                body.add(dataList);
-        });
-        body.removeIf(data -> data.isEmpty());
+        rowIterator.next().forEach( h -> header.add(h.getStringCellValue()));
+        header.removeIf( h -> h.isEmpty());
         table.setHeader(header);
-        table.setBody(body);
+        int dataTypeCounter = 0;
+        while(rowIterator.hasNext()) {
+            List<String> dataList = new ArrayList<>();
+            if(dataTypeCounter == 0){
+                rowIterator.next().forEach( cell -> {
+                    if(cell.getCellType() == CellType.STRING){
+                        dataList.add(cell.getStringCellValue());
+                        table.addDataType(DataType.STRING);
+                    }
+                    else if(cell.getCellType() == CellType.NUMERIC) {
+                        dataList.add(String.valueOf(cell.getNumericCellValue()));
+                        table.addDataType(DataType.INTEGER);
+                    }
+                    else if(cell.getCellType() == CellType.BOOLEAN) {
+                        dataList.add(String.valueOf(cell.getBooleanCellValue()));
+                        table.addDataType(DataType.BOLLEAN);
+                    }
+                });
+                dataList.removeIf(d -> d.isEmpty());
+                body.add(dataList);
+                dataTypeCounter++;
+            }
+            else {
+                rowIterator.next().forEach( cell -> {
+                    if(cell.getCellType() == CellType.STRING){
+                        dataList.add(cell.getStringCellValue());
+                    }
+                    else if(cell.getCellType() == CellType.NUMERIC) {
+                        dataList.add(String.valueOf(cell.getNumericCellValue()));
+                    }
+                    else if(cell.getCellType() == CellType.BOOLEAN) {
+                        dataList.add(String.valueOf(cell.getBooleanCellValue()));
+                    }
+                });
+                dataList.removeIf(d -> d.isEmpty());
+                body.add(dataList);
+                body.removeIf(d -> d.isEmpty());
+                table.setBody(body);
+            }
+        }
         return table;
     }
 
-    public void batchInsert(List<List<String>> bodyOfTable){
-
+    public void batchInsert(List<List<String>> bodyOfTable, String entityName, List<String> tableHeader) {
+        String data = this.sqlUtil.batchInsert(bodyOfTable, entityName, tableHeader);
+        System.out.println(data);
+        try {
+            this.dataSource.getConnection().createStatement().executeUpdate(data);
+        } catch (SQLException SQLEX) {
+            SQLEX.printStackTrace();
+            throw new RuntimeException(SQLEX.getMessage(), SQLEX.getCause());
+        }
     }
 }
